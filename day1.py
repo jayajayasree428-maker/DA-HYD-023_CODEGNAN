25f4c0ae47802c45d9052e0fff8edc4f93916019
jayajayasree428-maker <jayajaya428@gmail.com>
2026-07-28 15:12:41 +0530

    Tokens

---
+++ day1.py
@@ -0,0 +1,49 @@
+#print('hello codegnan')
+
+
+'''
+Tokens --> A smallest unit in program. There are 5 types of tokens..
+
+->Keywords
+->identifiers
+->Literals
+->Operators
+->Punctuators
+'''
+#Keywords -->These are reserved words in python. We have 35 Keywords...
+name = 'codegnan'
+print(name)
+email_id ='jayajayasree428@gmail.com'
+#print(email_id) #python is Case-Sensitive -->NameError
+#1swer = 23
+#print(1swer)
+#@defrrrr = 3.4 #No special characters
+
+#True = 45 #No Keywords
+
+batch = "da23"
+print(batch)
+
+#variable --> named memory location where we store the data, its a placeholder...
+batch = "cse"
+print(batch)
+
+batch = 23
+print(batch)
+
+#Literals --> These are constants (value(s)) -->number,character
+
+#Operators --> Operators performs specific operations -->7types
+
+length = 5
+breadth = 3
+area = length * breadth
+print(area)
+
+#Multiassignment of variables
+name,location,age = 'codegnan','Hyd',7
+print(name)
+print(location)
+print(age)
+
+ 
