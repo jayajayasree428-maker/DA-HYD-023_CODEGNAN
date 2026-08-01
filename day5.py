95d5d486140df80ea2cf098b8494deba3e057818
jayajayasree428-maker <jayajaya428@gmail.com>
2026-08-01 14:59:26 +0530

    elif usage

---
+++ day5.py
@@ -0,0 +1,101 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ============
+
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+Enter the age:
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+7
+9
+7 9
+codegnan DataAnalysis
+codegnan,DataAnalysis
+codegnan------->DataAnalysis
+codegnan DataAnalysis	7 9
+
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+DA-023 is in codegnan
+
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+DA-023 is in codegnan
+codegnan is in Hyderabad age is 7 years
+
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+DA-023 is in codegnan
+codegnan is in Hyderabad age is 7 years
+
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+DA-023 is in codegnan
+codegnan is in Hyderabad age is 7 years
+Traceback (most recent call last):
+  File "C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py", line 52, in <module>
+    print("His salary is %d"%(salary))
+TypeError: not all arguments converted during string formatting
+
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+DA-023 is in codegnan
+codegnan is in Hyderabad age is 7 years
+His salary is 25000
+His salary is 25000.000000
+
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+DA-023 is in codegnan
+codegnan is in Hyderabad age is 7 years
+His salary is 25000
+His salary is 25000.000000
+Traceback (most recent call last):
+  File "C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py", line 54, in <module>
+    print("His salary is %,if"%(salary))
+ValueError: unsupported format character ',' (0x2c) at index 15
+
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+DA-023 is in codegnan
+codegnan is in Hyderabad age is 7 years
+His salary is 24253
+His salary is 24253.256000
+Traceback (most recent call last):
+  File "C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py", line 54, in <module>
+    print("His salary is %,if"%(salary))
+ValueError: unsupported format character ',' (0x2c) at index 15
+>>> 
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+DA-023 is in codegnan
+codegnan is in Hyderabad age is 7 years
+His salary is 25000
+His salary is 25000.000000
+His salary is 25000.0
+>>> 
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+Traceback (most recent call last):
+  File "C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py", line 57, in <module>
+    print("(name) is in (place)".format(name,place))
+NameError: name 'name' is not defined
+>>> 
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+Traceback (most recent call last):
+  File "C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py", line 57, in <module>
+    print("{} is in {}".format(name,place)) #order matters
+NameError: name 'name' is not defined
+>>> 
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+Traceback (most recent call last):
+  File "C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py", line 57, in <module>
+    print("{} is in {}".format(name,place)) #order matters
+NameError: name 'name' is not defined
+>>> 
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+DA-023 is in codegnan
+codegnan is in Hyderabad age is 7 years
+His salary is 25000
+His salary is 25000.000000
+His salary is 25000.0
+codegnan is in Hyderabad
+(name) is in Hyderabad
+("Jayasree") is in codegnan
+>>> 
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
+marks(1-100):47
+user has failed,study again
+Enter the Student marks:
+================ RESTART: C:/Users/S jaya sri/Desktop/DA-HYD-23/day5.py ================
